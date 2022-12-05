f9501a966e2cd231d40f735abaca1a5272f12ffe
Michel Suzigan <mfsuzigan@gmail.com>
2022-12-05 13:39:23 -0300

    Requiring username instead of if as input; others

--- grabber.py
+++ grabber.py
@@ -32,7 +32,7 @@ def setupSpreadSheet(sheet):
     sheet.column_dimensions['C'].width = GENERAL_WIDTH
     sheet.column_dimensions['E'].width = PROFILE_IMAGE_WIDTH
 
-def getSpreadSheetName(pk):
+def getWorkbookName(pk):
     identifier = datetime.now().strftime("%y%m%d%H%M%S%f")[: -3]
     return f"insta_followers_account_{pk}_output--{identifier}.xlsx"
 
@@ -43,8 +43,8 @@ def getFollowers(pk, xIgAppId, cookie, nextMaxId):
     if nextMaxId and not nextMaxId == INITIAL_FOLLOWERS_REQUEST_MAX_ID:
         params = {"max_id": nextMaxId}
 
-    # response = requests.get(f"https://www.instagram.com/api/v1/friendships/{pk}/followers/", headers=headers, params=params)
-    response = requests.get("http://localhost:8000", headers=headers, params=params)
+    response = requests.get(f"https://www.instagram.com/api/v1/friendships/{pk}/followers/", headers=headers, params=params)
+    # response = requests.get("http://localhost:8000", headers=headers, params=params)
         
     try:
         # print(response.text)
@@ -54,7 +54,7 @@ def getFollowers(pk, xIgAppId, cookie, nextMaxId):
 
 def getArgs():
     parser = argparse.ArgumentParser()
-    parser.add_argument('pk', help='Target Instagram account ID')
+    parser.add_argument('username', help='Target Instagram username')
     parser.add_argument('xIgAppId', help='Your Instagram app ID')
     parser.add_argument('cookie', help='Instagram cookie from browser')
     return parser.parse_args()
@@ -75,22 +75,52 @@ def writeFollowers(followers, sheet, currentRow):
 
         print(f"Follower {element['username']} added to spreadsheet")
 
-def main():
-    args = getArgs()
-
+def getWorkbook():
     workbook = Workbook()
-    sheet = workbook.active
-    setupSpreadSheet(sheet)
+    setupSpreadSheet(workbook.active)
+    return workbook
 
-    nextMaxId = INITIAL_FOLLOWERS_REQUEST_MAX_ID
+def getIdForUsername(username, xIgAppId):
+    response = requests.get(f"https://i.instagram.com/api/v1/users/web_profile_info/?username={username}", headers={"x-ig-app-id": xIgAppId})
+
+    try:
+        return response.json()["data"]["user"]["id"]
+
+    except Exception:
+        print(f"Failed getting id for username {username}.")
+
+def saveFile(workbook, username):
+    filename = getWorkbookName(username)
+    workbook.save(filename=filename)
+    print(f"File {filename} saved")
+
+def main():
+    args = getArgs()    
+
+    rowCounter = 0
     followersCounter = 0
+    fileSavesCounter = 0
+
+    workbook = getWorkbook()
+    nextMaxId = INITIAL_FOLLOWERS_REQUEST_MAX_ID
+
+    pk = getIdForUsername(args.username, args.xIgAppId)
 
     while True:
-        followers = getFollowers(args.pk, args.xIgAppId, args.cookie, nextMaxId)
-        writeFollowers(followers, sheet, (followersCounter + 1))
+        followers = getFollowers(pk, args.xIgAppId, args.cookie, nextMaxId)
+        writeFollowers(followers, workbook.active, (rowCounter + 1))
+
+        rowCounter += len(followers["users"])
         followersCounter += len(followers["users"])
 
-        print(f"Followers added: {followersCounter}")
+        print(f"\nFollowers added: {followersCounter}\n")
+
+        if followersCounter >= (fileSavesCounter + 1) * 1000:
+            saveFile(workbook, args.username)
+            fileSavesCounter += 1
+
+            workbook = getWorkbook()
+            rowCounter = 0
 
         if "next_max_id" in followers:
             nextMaxId = followers["next_max_id"]
@@ -98,8 +128,8 @@ def main():
         else:
             break        
 
-    workbook.save(filename=getSpreadSheetName(args.pk))
-    print(f"\nFinished. Total of {followersCounter} followers added to spreadsheet.")
+    saveFile(workbook, args.username)
+    print(f"\nFinished.")
 
 if __name__ == '__main__':
     main()
